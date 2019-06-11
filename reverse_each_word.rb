9dd03b2489df82b902b4629ff618cc1635c66477
Carlos Velazquez <skamateria@gmail.com>
2019-06-11 11:33:37 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,6 @@
-def reverse_each_word("Hello, there and how are you?")
-  arrayify = "Hello, there and how are you?".chars
-end
+def reverse_each_word(sentence)
+  words_array = sentence.split(" ")
+  new_array = []
+  new_array = words_array.collect {|word| word.reverse }
+  new_array.join(" ")
 end
